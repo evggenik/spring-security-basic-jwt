9dc1099cefa705ec4cb3945d73a83ac235ad7a0c
Evgeny Buchelnikov <gru554@yandex.ru>
2024-08-09 22:45:34 +1000

    included test directory into git

---
+++ src/test/java/com/evggenn/spring_security_basic_jwt/SpringSecurityBasicJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evggenn.spring_security_basic_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
