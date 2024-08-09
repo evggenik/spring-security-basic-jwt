6c6dd7e39d75d17dbd50c5406d8529c418addeb1
Evgeny Buchelnikov <gru554@yandex.ru>
2024-08-09 19:56:54 +1000

    created project with basic settings

---
+++ src/main/java/com/evggenn/spring_security_basic_jwt/SpringSecurityBasicJwtApplication.java
@@ -0,0 +1,13 @@
+package com.evggenn.spring_security_basic_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBasicJwtApplication.class, args);
+	}
+
+}
